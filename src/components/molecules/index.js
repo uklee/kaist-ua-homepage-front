6150b796b1f2e85a4e60c35c3e46766efbe5c831
김승호 <david@Natrium.local>
2020-03-14 18:34:29 +0900

    feat(home): break down carousel component

--- src/components/molecules/index.js
+++ src/components/molecules/index.js
@@ -1,1 +1,1 @@
-export { default as HomeCarousel } from "./HomeCarousel";
+export { default as CarouselItem } from "./CarouselItem";
--- src/components/templates/HomeContent.js
+++ src/components/templates/HomeContent.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container } from "react-bootstrap";
-import { HomeCarousel } from "../molecules";
+import { HomeCarousel } from "../organisms";
 
 const HomeContent = () => {
   return (
     <Container className="flex-grow-1">
+      <br />
       <HomeCarousel />
     </Container>
   );
