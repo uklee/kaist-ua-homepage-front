640871ae60e9a891a3583b075c5fa1f990589c2a
김승호 <david@Natrium.local>
2020-03-16 02:50:20 +0900

    feat(noticeDetail): create notice detail page

---
+++ src/components/pages/NoticeDetail.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import { Header, Footer } from "../organisms";
+import { NoticeDetailContent } from "../templates";
+
+const NoticeDetail = () => {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        backgroundColor: "#eee",
+        fontFamily: "NanumSquare"
+      }}
+      className="d-flex flex-column"
+    >
+      <Header notice={true} />
+      <NoticeDetailContent />
+      <Footer />
+    </div>
+  );
+};
+
+export default NoticeDetail;
---
+++ src/components/templates/NoticeDetailContent.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+import "./NoticeDetailContent.scss";
+
+const NoticeDetailContent = () => {
+  return <Container className="flex-grow-1 notice-detail-content"></Container>;
+};
+
+export default NoticeDetailContent;
