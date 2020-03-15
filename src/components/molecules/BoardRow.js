5ae27e47f7b42ca8e18c4782d8d0fffb6b777338
김승호 <david@Natrium.local>
2020-03-16 02:19:16 +0900

    hotfix(board): modify board structure to bootstrap grid

--- src/components/molecules/BoardRow.js
+++ src/components/molecules/BoardRow.js
@@ -1,15 +1,22 @@
 import React from "react";
+import { Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import "./BoardRow.scss";
 
 const BoardRow = props => {
   return (
-    <Link to="/" className="d-flex flex-row board-row">
-      <p className="row-author">{props.author}</p>
-      <p className="flex-grow-1 row-title">{props.title}</p>
-      <div className="row-date d-flex flex-row-reverse">{props.date}</div>
-    </Link>
+    <Row as={Link} to="/" className="board-row">
+      <Col md="2" lg="3" className="row-author">
+        {props.author}
+      </Col>
+      <Col md="8" lg="6" className="row-title">
+        {props.title}
+      </Col>
+      <Col md="2" lg="3" className="row-date">
+        {props.date}
+      </Col>
+    </Row>
   );
 };
 
