01fee1424d6defc5eacb06684cabb084050a8796
CoderLulu <yoonjs0510@kaist.ac.kr>
2020-04-12 16:31:32 +0900

    feat(route): Redirect any path to mainpage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "./App.css";
 import {
@@ -13,12 +13,13 @@ function App() {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/main" component={MainPage} />
         <Route path="/post/:postId" component={DetailPage} />
         <Route path="/bulletin/:bulletinId" component={BulletinPage} />
         <Route path="/user/bulletin/:bulletinId" component={BulletinPage} />
         <Route path="/edit" component={EditPage} />
         <Route path="/user/edit" component={EditPage} />
-        <Route path="/" component={MainPage} />
+        <Redirect to="/main" />
       </Switch>
     </BrowserRouter>
   );
