25c4205764d040b2817f1e9eb2f88420f7eb4afd
winningarc <shokim01@gmail.com>
2020-08-26 17:14:37 +0900

    fix(board): remove link in empty rows

--- src/components/molecules/BoardRow.js
+++ src/components/molecules/BoardRow.js
@@ -5,19 +5,23 @@ import { Link } from "react-router-dom";
 import "./BoardRow.scss";
 
 const BoardRow = props => {
-  return (
-    <Row as={Link} to={`/web/post/${props.id}`} className="board-row">
-      <Col md="2" lg="3" className="row-author">
-        {props.author}
-      </Col>
-      <Col md="8" lg="5" className="row-title">
-        {props.title}
-      </Col>
-      <Col md="2" lg="4" className="row-date">
-        {props.createdAt ? props.createdAt.split(/T|.000Z/)[0] : null}
-      </Col>
-    </Row>
-  );
+  if (props.title) {
+    return (
+      <Row as={Link} to={`/web/post/${props.id}`} className="board-row">
+        <Col md="2" lg="3" className="row-author">
+          {props.author}
+        </Col>
+        <Col md="8" lg="5" className="row-title">
+          {props.title}
+        </Col>
+        <Col md="2" lg="4" className="row-date">
+          {props.createdAt ? props.createdAt.split(/T|.000Z/)[0] : null}
+        </Col>
+      </Row>
+    );
+  } else {
+    return <Row className="board-row" />;
+  }
 };
 
 export default BoardRow;
