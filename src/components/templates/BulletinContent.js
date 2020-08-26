5e8537c4597ec90c24e8c733f67b59f0befcfee2
winningarc <shokim01@gmail.com>
2020-08-26 17:24:43 +0900

    fix: bulletin loading screen

--- src/components/templates/BulletinContent.js
+++ src/components/templates/BulletinContent.js
@@ -1,12 +1,18 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Spinner } from "react-bootstrap";
 import "./BulletinContent.scss";
 import { BulletinList } from "../organisms";
 import { BulletinHeader, BulletinFooter } from "../molecules";
 import { isEmpty } from "lodash";
 
 const BulletinContent = ({ bulletins, bulletinId, posts, isUser }) => {
-  if (isEmpty(bulletins)) return <div>로딩중...</div>;
+  if (isEmpty(bulletins))
+    return (
+      <Container className="flex-grow-1 bulletin-loading">
+        <Spinner animation="border" />
+        <div>Loading...</div>
+      </Container>
+    );
 
   const bulletin = bulletins.find(bulletin => `${bulletin.id}` === bulletinId);
 
