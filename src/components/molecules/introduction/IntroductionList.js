b141ebb42e0b23e5b3b15d3e4957dfeead792af7
Heo Hyeon <heohyun73@gmail.com>
2021-07-25 17:18:40 +0900

    fix: introduction page renewal (2021erc)

--- src/components/molecules/introduction/IntroductionList.js
+++ src/components/molecules/introduction/IntroductionList.js
@@ -9,10 +9,6 @@ const IntroductionList = () => {
       title: "총학생회 소개"
     },
     {
-      link: "#2",
-      title: "회장단 인사말"
-    },
-    {
       subTabs: true,
       link: "#3_1",
       title: "중앙집행위원회"
@@ -32,21 +28,17 @@ const IntroductionList = () => {
       title: "총학생회 소개"
     },
     {
-      link: "#2",
-      title: "회장단 인사말"
-    },
-    {
       link: "#3_1",
       title: "중앙집행위원회"
     },
     {
       link: "#3_2",
-      title: "사무국",
+      title: "서기실",
       isSubTab: true
     },
     {
       link: "#3_3",
-      title: "복지국",
+      title: "집행지원실",
       isSubTab: true
     },
     {
@@ -56,32 +48,22 @@ const IntroductionList = () => {
     },
     {
       link: "#3_5",
-      title: "문화기획국",
+      title: "복지국",
       isSubTab: true
     },
     {
       link: "#3_6",
-      title: "국제사무국",
+      title: "사무국",
       isSubTab: true
     },
     {
       link: "#3_7",
-      title: "협력국",
+      title: "디자인국",
       isSubTab: true
     },
     {
       link: "#3_8",
-      title: "집행지원실",
-      isSubTab: true
-    },
-    {
-      link: "#3_9",
-      title: "디자인정보팀",
-      isSubTab: true
-    },
-    {
-      link: "#3_10",
-      title: "회계팀",
+      title: "정보국",
       isSubTab: true
     },
     {
