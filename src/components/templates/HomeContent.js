ce0a6ee1733568fe176aafd8ecb9d79bcc148d1e
uklee <arn0287@mail.ru>
2022-02-23 19:31:05 +0900

    changed ad banner

--- src/components/templates/HomeContent.js
+++ src/components/templates/HomeContent.js
@@ -20,12 +20,32 @@ const HomeContent = ({ BoardModuleInfos }) => {
     <Container className="flex-grow-1 home-content">
       <HomeCarousel className="module" />
       <Row>{boardModules}</Row>
-      <div>
-        <img
-          className="bottom-banner"
-          src="https://kaistua-web.s3.ap-northeast-2.amazonaws.com/ad-banner.png"
-          alt="dfdf"
-        />
+      <div class="ad-banner">
+        <div class="ad-box">
+          <a href="https://student.kaist.ac.kr/wiki/">
+            <img
+              className="ad"
+              src="https://kaistua-web.s3.ap-northeast-2.amazonaws.com/logo-kaipedia.png"
+              alt="ad here"
+            />
+          </a>
+        </div>
+        <div class="ad-box">
+          <a href="https://ara.kaist.ac.kr">
+            <img
+              className="ad"
+              src="https://kaistua-web.s3.ap-northeast-2.amazonaws.com/logo-ara.png"
+              alt="ad here"
+            />
+          </a>
+        </div>
+        <div class="ad-box">
+          <img
+            className="ad"
+            src="https://kaistua-web.s3.ap-northeast-2.amazonaws.com/ad-legochembio.jpg"
+            alt="ad here"
+          />
+        </div>
       </div>
     </Container>
   );
