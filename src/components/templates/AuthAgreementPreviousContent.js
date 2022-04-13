bca862c5384a010e77aa1fe9e75bfa189b58e8ff
uklee <arn0287@mail.ru>
2022-04-14 05:28:12 +0900

    Term updated

--- src/components/templates/AuthAgreementPreviousContent.js
+++ src/components/templates/AuthAgreementPreviousContent.js
@@ -2,6 +2,7 @@ import React from "react";
 
 
 import Terms1 from "../molecules/terms/terms_20210908.js"
+import Terms2 from "../molecules/terms/terms_20220414.js"
 
 const AuthAgreementPreviousContent = () => {
   return (
@@ -9,6 +10,10 @@ const AuthAgreementPreviousContent = () => {
       <details>
         <summary>홈페이지 최초 운영일 ~ 2021.09.07 적용지침</summary>
         <Terms1 />
+      </details>
+      <details>
+        <summary>2021.09.08 ~ 2022.04.13 적용지침</summary>
+        <Terms2 />
       </details>
     </div>
   );
